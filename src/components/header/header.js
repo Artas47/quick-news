d767490a4b5859349a14883b7dc7e4590d059869
artas <artasss47@gmail.com>
2019-12-17 14:03:31 +0100

    added logo

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,6 +6,7 @@ import * as Styled from './header.styles';
 export const Header = () => {
   return (
     <Styled.Header>
+      <Styled.LogoAndName />
       <SearchBar />
       <LanguageChoose />
     </Styled.Header>
--- src/components/header/header.styles.js
+++ src/components/header/header.styles.js
@@ -1,10 +1,21 @@
 import styled from 'styled-components';
+import { ReactComponent as Logo } from '../../assets/logo-name.svg';
 
 export const Header = styled.div`
   width: 100%;
   z-index: 999;
+  height: 10rem;
   background-color: white;
   padding: 3rem;
   display: flex;
   justify-content: center;
+  position: sticky;
+  top: 0;
+`;
+
+export const LogoAndName = styled(Logo)`
+  position: absolute;
+  top: 50%;
+  left: 3rem;
+  transform: translate(0, -50%);
 `;
